f25e235759a42623373b0e3a4a1879f6a0cdbefd
Roman Gusiev <gusev.ry@gmail.com>
2018-09-19 13:53:14 -0700

    deck storage done

---
+++ data/deckStorage.js
@@ -0,0 +1,18 @@
+const Deck = require('./deck');
+const uuid = require('uuid/v4');
+
+class DeckStorage {
+    constructor() {
+        this.decks = {};
+    }
+    addDeck() {
+        let id = uuid();
+        this.decks[id] = new Deck();
+        return id;
+    }
+    getDeck(id) {
+        if (!this.decks[id]) return {error: 'No deck with given id'}
+        return this.decks[id];
+    }
+}
+module.exports = new DeckStorage();
